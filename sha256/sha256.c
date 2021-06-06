b14f0165f2d6e506136eef528275347d373e45df
emilelmarsson <emil.elmarsson@gmail.com>
2021-06-06 22:07:19 +0200

    Moved constans from header to source

--- sha256/sha256.c
+++ sha256/sha256.c
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
+#include <stdint.h>
+
+#define WORD_SIZE 32
+#define WORD_SIZE_BYTES 4
+#define BLOCK_SIZE 512
+#define BLOCK_SIZE_BYTES 64
+#define WORDS_IN_BLOCK 16
+#define WORDS_IN_WORKING_SCHEDULE 64
+#define ROUNDS 64 // "Rounds of compression"
+#define BITS_OF_ZERO_PADDING 448
 
 // Choice function; if a is 1 pick b, otherwise pick c.
 static inline uint32_t ch(uint32_t a, uint32_t b, uint32_t c){
@@ -57,8 +67,8 @@ static uint32_t* preprocessing(char *message, uint64_t N, uint64_t l, uint64_t k
 
     memcpy(M, message, l / 8); // Read message
 
-    #if defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN // If little-endian, swap the bytes. SHA-2 is specified for big-endian integers.
-        for(int i=0; i<N*16; i++){
+    #if defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN // gcc compile-time little-endian check
+        for(int i=0; i<N*16; i++){ // If little-endian, swap the bytes. SHA-2 is specified for big-endian integers.
             M[i] = swap_uint32(M[i]);
         }
     #endif
@@ -103,66 +113,79 @@ static uint32_t H[8] = {
     0x1f83d9ab,
     0x5be0cd19};
 
+// First 32 bits of the fractional parts of the cube roots of the first 64 prime numbers.
+static const uint32_t K[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
+
+void sha256(char *message){
+    uint64_t l = strlen(message) * 8; // Message length (in bits)
+    uint64_t k = (BITS_OF_ZERO_PADDING - (l % BLOCK_SIZE) - 1) % BLOCK_SIZE; // Bits of zero-padding (final 64 bits contain the length of the message)
+    uint64_t N = (l / BLOCK_SIZE) + 1; // Message length (in 512-bit blocks)
+
+    uint32_t* M = preprocessing(message, N, l, k);
+
+    // Hash computation
+    for(int i=0; i<N; i++){
+        uint32_t *W = prepare_message_schedule(&M[i * WORDS_IN_BLOCK]);
+
+        // Initialize the eight working variables with first/intermediate hash value.
+        uint32_t a = H[0];
+        uint32_t b = H[1];
+        uint32_t c = H[2];
+        uint32_t d = H[3];
+        uint32_t e = H[4];
+        uint32_t f = H[5];
+        uint32_t g = H[6];
+        uint32_t h = H[7];
+
+        // Compression function
+        for(int t=0; t<ROUNDS; t++){
+            uint32_t T1 = h + Sigma1(e) + ch(e, f, g) + K[t] + W[t];
+            uint32_t T2 = Sigma0(a) + maj(a, b, c);
+            h = g;
+            g = f;
+            f = e;
+            e = d + T1;
+            d = c;
+            c = b;
+            b = a;
+            a = T1 + T2;
+        }
+
+        // Calculate intermediate hash value
+        H[0] = a + H[0];
+        H[1] = b + H[1];
+        H[2] = c + H[2];
+        H[3] = d + H[3];
+        H[4] = e + H[4];
+        H[5] = f + H[5];
+        H[6] = g + H[6];
+        H[7] = h + H[7];
+
+        free(W);
+    }
+
+    // Print final hash values
+    for(int i=0; i<8; i++){
+        printf("%08x", H[i]);
+    }
+    printf("\n");
+
+    free(M);
+}
+
 int main(int argc, char *argv[]){
     if(argc < 2 || argc > 2){
         printf("One argument expected.\n");
     }else{
-        char *message = argv[1]; // Message
-
-        uint64_t l = strlen(message) * 8; // Message length (in bits)
-        uint64_t k = (BITS_OF_ZERO_PADDING - (l % BLOCK_SIZE) - 1) % BLOCK_SIZE; // Bits of zero-padding (final 64 bits contain the length of the message)
-        uint64_t N = (l / BLOCK_SIZE) + 1; // Message length (in 512-bit blocks)
-
-        uint32_t* M = preprocessing(message, N, l, k);
-
-        // Hash computation
-        for(int i=0; i<N; i++){
-            uint32_t *W = prepare_message_schedule(&M[i * WORDS_IN_BLOCK]);
-
-            // Initialize the eight working variables with first/intermediate hash value.
-            uint32_t a = H[0];
-            uint32_t b = H[1];
-            uint32_t c = H[2];
-            uint32_t d = H[3];
-            uint32_t e = H[4];
-            uint32_t f = H[5];
-            uint32_t g = H[6];
-            uint32_t h = H[7];
-
-            // Compression function
-            for(int t=0; t<ROUNDS; t++){
-                uint32_t T1 = h + Sigma1(e) + ch(e, f, g) + K[t] + W[t];
-                uint32_t T2 = Sigma0(a) + maj(a, b, c);
-                h = g;
-                g = f;
-                f = e;
-                e = d + T1;
-                d = c;
-                c = b;
-                b = a;
-                a = T1 + T2;
-            }
-
-            // Calculate intermediate hash value
-            H[0] = a + H[0];
-            H[1] = b + H[1];
-            H[2] = c + H[2];
-            H[3] = d + H[3];
-            H[4] = e + H[4];
-            H[5] = f + H[5];
-            H[6] = g + H[6];
-            H[7] = h + H[7];
-
-            free(W);
-        }
-
-        // Print final hash values
-        for(int i=0; i<8; i++){
-            printf("%08x", H[i]);
-        }
-        printf("\n");
-
-        free(M);
+        sha256(argv[1]);
     }
     return 0;
 }
